649e450409e22aadc3597c9f77eae71f1f69d9ad
Archaversine <adam.brohl.w@gmail.com>
2024-04-16 09:20:36 -0500

    Upload to Git

---
+++ src/client/java/dev/lmao/LmaoModClient.java
@@ -0,0 +1,10 @@
+package dev.lmao;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LmaoModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
