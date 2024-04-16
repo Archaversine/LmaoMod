c08813c18ca1fbcf3606e5e153c4b18ecdfc159e
Archaversine <adam.brohl.w@gmail.com>
2024-04-16 12:08:18 -0500

    Added `MOD_ID` variable

--- src/main/java/dev/lmao/LmaoMod.java
+++ src/main/java/dev/lmao/LmaoMod.java
@@ -6,10 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LmaoMod implements ModInitializer {
+    // Do not change this unless you know what you are doing
+    public static final String MOD_ID = "lmaomod";
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("lmaomod");
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
